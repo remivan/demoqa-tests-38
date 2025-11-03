504b2c3d0812cf2112e534fc47155337aeca8eef
Irina <dead.rescuer@yandex.com>
2025-11-03 13:49:16 +0300

    added allure

--- src/test/java/tests/DemoqaTests.java
+++ src/test/java/tests/DemoqaTests.java
@@ -2,7 +2,6 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
