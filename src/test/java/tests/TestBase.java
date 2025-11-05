4c290ff663ef07ba0a17bc02fbd86990fd47fbd4
Irina <dead.rescuer@yandex.com>
2025-11-05 17:19:09 +0300

    updated configuration

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -18,7 +18,7 @@ public class TestBase {
         Configuration.pageLoadStrategy = "eager";
 
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion", "142.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
